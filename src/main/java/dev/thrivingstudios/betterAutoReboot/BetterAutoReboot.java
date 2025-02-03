d3cbeed1cab44e769229054d6defe398e69cf954
Hector <102716847+hectorwithc@users.noreply.github.com>
2025-02-03 16:29:07 +0100

    Initial code.

---
+++ src/main/java/dev/thrivingstudios/betterAutoReboot/BetterAutoReboot.java
@@ -0,0 +1,17 @@
+package dev.thrivingstudios.betterAutoReboot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterAutoReboot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
